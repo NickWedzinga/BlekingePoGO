4d76d7c77a686ef79a74bfe4d74d451b5d8ca573
NickWedzinga <oxay@live.com>
2018-07-15 18:38:31 +0200

    Rerollerboi

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ EventHandler.startUp()
 #Read message every loop,  act accordingly
 EventHandler.checkMessages()
 #Infinite blocking loop
-EventHandler.client.run('NDM1ODg1MDIyNTA0MDI2MTEy.Dbineg.scoWXTLQWPsJc64XXv8daggHk1U')
+EventHandler.client.run('')
 EventHandler.client.close()
 
 # TODO:
