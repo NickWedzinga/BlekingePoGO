c75b2b1564e1ab652f295dd332446dbe6f74c82a
NickWedzinga <oxay@live.com>
2018-07-18 00:56:37 +0200

    main TODO comments updated

--- Main.py
+++ Main.py
@@ -22,13 +22,12 @@ EventHandler.client.close()
 
 # LEADERBOARDS---------------------------------------------
 # Lägg till Battle Girl, gitignore, skapa .txt filer på PI, lägg till i help, loop through new files in admin claim
-# lägg till i delete if case (skriva om till match in list)
+# lägg till i delete list
 
 # GRATULERAT, inte korrekt svenska?
 
 
 # DELETE-----------------------------------------------------
-# crash vid delete, kraschen är kring local leaderboardList i funktionen leaderboard
 
 
 # REFRESH----------------------------------------------------
