4e7e80f7c1af794ab647311c48bfa6c7969bfc75
NickWedzinga <oxay@live.com>
2019-11-07 20:53:34 +0100

    Added error-handling for @everyone mentions and missing commands. Created ErrorHandling class

---
+++ ErrorHandling.py
@@ -0,0 +1,43 @@
+from Instance import bot
+from discord.ext import commands
+import Common
+
+
+class ErrorHandling(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if '@EVERYONE' in message.content.upper() and 342771363884302339 not in [role.id for role in message.author.roles]:
+            for role in message.guild.roles:
+                if 435908470936698910 == role.id:
+                    await message.channel.send("Ping! %s" % role.mention)
+                elif 342771363884302339 == role.id:
+                    await message.channel.send("Ping! %s" % role.mention)
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send(f"Command [{ctx.invoked_with}] was not found.")
+        for dev in Common.developers:
+            user = ctx.bot.get_user(dev)
+            await user.send(f"""GENERIC error in command: {error}""")
+
+    # global check that applies to all commands
+    # checks if commands was sent from an expected channel
+    @commands.Cog.listener()
+    async def global_channel_check(self, ctx):  # TODO: Can this be one-lined without losing readability?
+        # reads as, message was sent in a command channel and a command, other than claim, was not sent in support channel
+        if str(ctx.message.channel) not in Common.command_channel_list:
+            return False
+        elif str(ctx.invoked_with) != "claim":
+            if str(ctx.message.channel) == Common.command_channel_list[2]:
+                return False
+            return True
+        else:
+            return True
+
+
+def setup(bot):
+    bot.add_cog(ErrorHandling(bot))
--- Instance.py
+++ Instance.py
@@ -1,34 +1,13 @@
 import discord
 from discord.ext import commands as discordcommands
-import Common
 
 # list of all cogs to add
-initial_extensions = ['commands.test', 'commands.information', 'commands.support', 'commands.leaderboards']
+initial_extensions = ['commands.test', 'commands.information', 'commands.support', 'commands.leaderboards',
+                      'ErrorHandling']
 
 bot = discordcommands.Bot(command_prefix="?")
 for extension in initial_extensions:
     bot.load_extension(extension)
-
-# global check that applies to all commands
-# checks if commands was sent from an expected channel
-@bot.check
-async def global_channel_check(ctx):  # TODO: Can this be one-lined without losing readability?
-    # reads as, message was sent in a command channel and a command, other than claim, was not sent in support channel
-    if str(ctx.message.channel) not in Common.command_channel_list:
-        return False
-    elif (str(ctx.invoked_with) != "claim"):
-        if (str(ctx.message.channel) == Common.command_channel_list[2]):
-            return False
-        return True
-    else:
-        return True
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    for dev in Common.developers:
-        user = ctx.bot.get_user(dev)
-        await user.send(f"""GENERIC error in command: {error}""")
 
 
 def startup():
