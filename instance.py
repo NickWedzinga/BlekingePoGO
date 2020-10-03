9e4d0d348e341b6894a14d9971713c9ea153359f
Nick Wedzinga <oxay@live.com>
2020-10-03 08:03:39 +0200

    #147: Added support for intents
    Closes: #147

--- instance.py
+++ instance.py
@@ -1,13 +1,15 @@
 import discord
 from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix="?", intents=intents, case_insensitive=True)
+
 initial_extensions = ['utils.global_error_manager', 'testing.integration.integration_manager', 'sneaselcommands.list',
                       'sneaselcommands.support', 'sneaselcommands.leaderboards', 'sneaselcommands.ranks',
                       'sneaselcommands.configure', 'sneaselcommands.dex', 'sneaselcommands.refresh',
                       'sneaselcommands.raids.raid', 'sneaselcommands.raids.close', 'sneaselcommands.raids.update',
                       'sneaselcommands.raids.status', 'sneaselcommands.raids.raids']
-
-bot = commands.Bot(command_prefix="?", case_insensitive=True)
 for extension in initial_extensions:
     bot.load_extension(extension)
 
