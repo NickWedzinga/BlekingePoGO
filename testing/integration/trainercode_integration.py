147658a97264adecea02fdd94ca35c97503fbc2c
Nick Wedzinga <oxay@live.com>
2020-11-28 19:14:03 +0100

    fix: Fixes trainer codes starting with 0 not working

--- testing/integration/trainercode_integration.py
+++ testing/integration/trainercode_integration.py
@@ -17,18 +17,25 @@ async def call_trainercode_tests(bot, ctx):
 async def add_and_remove_code_test(bot, ctx):
     trainercode_command = bot.get_command("trainercode")
 
+    await ctx.invoke(trainercode_command, "remove")
     maybe_code_in_db = check_in_db(ctx)
-    assert(not maybe_code_in_db)
+    assert (not maybe_code_in_db)
 
-    await ctx.invoke(trainercode_command, "111122223333")
-
-    maybe_code_in_db = check_in_db(ctx)
-    assert maybe_code_in_db
+    await upsert_code_and_assert(ctx, trainercode_command, "111122223333")
+    await upsert_code_and_assert(ctx, trainercode_command, "000011112222")
+    await upsert_code_and_assert(ctx, trainercode_command, "0000", "1111", "2222")
 
     await ctx.invoke(trainercode_command, "remove")
 
     maybe_code_in_db = check_in_db(ctx)
-    assert(not maybe_code_in_db)
+    assert not maybe_code_in_db
+
+
+async def upsert_code_and_assert(ctx, command, *code):
+    await ctx.invoke(command, *code)
+    maybe_code_in_db = check_in_db(ctx)
+    assert len(maybe_code_in_db) == 1
+    assert maybe_code_in_db[0].get("code") == "".join(code)
 
 
 def check_in_db(ctx) -> list:
@@ -37,7 +44,6 @@ def check_in_db(ctx) -> list:
         where_key="user_id",
         where_value=ctx.author.id
     )).all(as_dict=True)
-
 
 
 async def run_tests(bot, ctx):
